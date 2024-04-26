e0c17147d8f3b61fea928f3082e84ec4dd2c4964
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-04-26 18:46:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -84,7 +84,7 @@ async def Lazy_start():
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
     logger.info(LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__))
-    try: await self.send_message(LOG_CHANNEL, text=LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__), disable_web_page_preview=True)   
+    try: await LazyPrincessBot.send_message(LOG_CHANNEL, text=LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__), disable_web_page_preview=True)   
     except Exception as e: logger.warning(f"Bot Isn't Able To Send Message To LOG_CHANNEL \n{e}")
     if WEBHOOK is True:
         app = web.AppRunner(await web_server())
