7905c379c23bd1a21f8ddc17e4af48f8ed6616e5
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-05-18 08:14:53 +0530

    Update join_req.py

--- clone_plugins/join_req.py
+++ clone_plugins/join_req.py
@@ -5,8 +5,8 @@ from info import ADMINS, AUTH_CHANNEL, SYD_CHANNEL
 
 @Client.on_chat_join_request(filters.chat(AUTH_CHANNEL))
 async def join_reqs(client, message: ChatJoinRequest):
-  if not await db.find_join_req(message.from_user.id):
-    await db.add_join_req(message.from_user.id)
+  if not await bd.find_join_req(message.from_user.id):
+    await bd.add_join_req(message.from_user.id)
 
 @Client.on_message(filters.command("delreq") & filters.private)
 id = bot.me.id
@@ -16,7 +16,7 @@ id = bot.me.id
         await message.reply_text("ᴏɴʟʏ ᴏᴡɴᴇʀ ᴄᴏᴍᴍᴀɴᴅ❗")
         return
 async def del_requests(client, message):
-    await db.del_join_req()    
+    await bd.del_join_req()    
     await message.reply("<b>⚙ ꜱᴜᴄᴄᴇꜱꜱғᴜʟʟʏ ᴄʜᴀɴɴᴇʟ ʟᴇғᴛ ᴜꜱᴇʀꜱ ᴅᴇʟᴇᴛᴇᴅ</b>")
   
 @Client.on_chat_join_request(filters.chat(SYD_CHANNEL))
