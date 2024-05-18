17afd4a4f322b62d7de138bab3d5fb295669cc90
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-05-18 08:08:13 +0530

    Update join_req.py

--- clone_plugins/join_req.py
+++ clone_plugins/join_req.py
@@ -8,7 +8,13 @@ async def join_reqs(client, message: ChatJoinRequest):
   if not await db.find_join_req(message.from_user.id):
     await db.add_join_req(message.from_user.id)
 
-@Client.on_message(filters.command("delreq") & filters.private & filters.user(ADMINS))
+@Client.on_message(filters.command("delreq") & filters.private)
+id = bot.me.id
+    owner = mongo_db.bots.find_one({'bot_id': id})
+    ownerid = int(owner['user_id'])
+    if ownerid != message.from_user.id:
+        await message.reply_text("ᴏɴʟʏ ᴏᴡɴᴇʀ ᴄᴏᴍᴍᴀɴᴅ❗")
+        return
 async def del_requests(client, message):
     await db.del_join_req()    
     await message.reply("<b>⚙ ꜱᴜᴄᴄᴇꜱꜱғᴜʟʟʏ ᴄʜᴀɴɴᴇʟ ʟᴇғᴛ ᴜꜱᴇʀꜱ ᴅᴇʟᴇᴛᴇᴅ</b>")
