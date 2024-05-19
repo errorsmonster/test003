2a95f41d85c34c0c4cb64d0bae07f4665454fc22
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-05-19 10:58:02 +0530

    Update customize.py

--- clone_plugins/customize.py
+++ clone_plugins/customize.py
@@ -1,1 +1,28 @@
-hi
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
+
+
+
+
+@Client.on_message(filters.command("customize") & filters.private)
+async def sydoo(bot, message):
+    id = bot.me.id
+    owner = mongo_db.bots.find_one({'bot_id': id})
+    ownerid = int(owner['user_id'])
+    if ownerid != message.from_user.id:
+        await message.reply_text("ᴏɴʟʏ ᴏᴡɴᴇʀ ᴄᴏᴍᴍᴀɴᴅ❗")
+        return
+    text = await message.reply_text(text="<code>Gᴇᴛᴛɪɴɢ ɪɴꜰᴏ.....</code>", disable_web_page_preview=True) 
+    await asyncio.sleep(0.6)
+    await text.edit_text(
+        text=f"<b>Link :-</b>\n\n<code>he[0]}</code>",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup( [[
+            InlineKeyboardButton("Oᴩᴇɴ Lɪɴᴋ", callback_data="start"),
+            InlineKeyboardButton("βᴏᴛ𝘴 🏞️", callback_data="start"),
+            InlineKeyboardButton("Sʜᴀʀᴇ Lɪɴᴋ", callback_data="help")
+            ],[
+            InlineKeyboardButton("✗ Cꪶꪮ𝘴ꫀ ✗", callback_data="close")
+            ]])
+    )
+    
