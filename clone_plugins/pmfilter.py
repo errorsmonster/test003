65925506a9971a758f0d06540234d306f23bd2a1
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-05-19 15:26:28 +0530

    Update pmfilter.py

--- clone_plugins/pmfilter.py
+++ clone_plugins/pmfilter.py
@@ -1598,6 +1598,17 @@ async def cb_handler(client: Client, query: CallbackQuery):
            return await forc_id.reply_text(
                      "<b>Pʀᴏᴄᴇꜱꜱ ᴄᴀɴᴄᴇʟᴇᴅ 😮‍💨 !</b>",
                      reply_markup=reply_markup)
+        elif not forc_id.forward_date:
+        return await forc_id.reply("**This is not a forward message**")
+     else:
+        forc_ids = forc_id.forward_from_chat.id
+        title = forc_id.forward_from_chat.title
+        username = forc_id.forward_from_chat.username
+        username = "@" + username if username else "private"
+     forc = await db.add_channel(user_id, forc_ids, title, username)
+     await query.message.reply_text(
+        "<b>Successfully updated</b>" if chat else "<b>This channel already added</b>",
+        reply_markup=InlineKeyboardMarkup(buttons))
 
     elif query.data == "qr_info":
         buttons = [[
