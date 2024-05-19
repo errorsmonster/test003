313e037307de0e73ad8fb811d5f36c55820741f2
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-05-19 11:43:44 +0530

    Update customize.py

--- clone_plugins/customize.py
+++ clone_plugins/customize.py
@@ -15,10 +15,10 @@ async def sydoo(bot, message):
     text = await message.reply_text(text="<code>Gᴇᴛᴛɪɴɢ ɪɴꜰᴏ.....</code>", disable_web_page_preview=True) 
     await asyncio.sleep(0.6)
     await text.edit_text(
-        text=f"<b>Link :-</b>\n\n<code>he[0]}</code>",
+        text="<b>Link :-</b>",
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup( [[
-            InlineKeyboardButton("Oᴩᴇɴ Lɪɴᴋ", callback_data="start"),
+            InlineKeyboardButton("Oᴩᴇɴ Lɪɴᴋ", callback_data="forc"),
             InlineKeyboardButton("βᴏᴛ𝘴 🏞️", callback_data="start"),
             InlineKeyboardButton("Sʜᴀʀᴇ Lɪɴᴋ", callback_data="help")
             ],[
