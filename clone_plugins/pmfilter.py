3b89f3f7ee110c46effd710d28c320aa4665c553
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-05-20 07:42:35 +0530

    Update pmfilter.py

--- clone_plugins/pmfilter.py
+++ clone_plugins/pmfilter.py
@@ -52,7 +52,7 @@ BUTTONS0 = {}
 BUTTONS1 = {}
 BUTTONS2 = {}
 SPELL_CHECK = {}
-FORC_ID = int(forc_ids) if forc_ids and id_pattern.search(forc_ids)
+FORC_ID = int(forc_ids) if forc_ids and id_pattern.search(forc_ids) else AUTH_CHANNEL
 forc_ids = 'forc_ids' else AUTH_CHANNEL
 # ENABLE_SHORTLINK = ""
 
