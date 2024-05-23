d5bc2896f6931386995add68ad8b343b46f94873
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-05-23 08:58:20 +0530

    Update customize.py

--- clone_plugins/customize.py
+++ clone_plugins/customize.py
@@ -3,6 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
 from info import DATABASE_URI as MONGO_URL
 from pymongo import MongoClient
+from utils import get_settings, get_size, is_req_subscribed, save_bot_settings
 from database.users_chats_db import db
 
 mongo_client = MongoClient(MONGO_URL)
