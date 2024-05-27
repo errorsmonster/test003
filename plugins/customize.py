60330a5f0cb727992804cb21b931f34d3b26d77d
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-05-27 21:07:51 +0530

    Update customize.py

--- plugins/customize.py
+++ plugins/customize.py
@@ -37,7 +37,7 @@ async def settings_query(bot, query):
      bots = await db.get_bots(user_id)
      for bot in bots:
         buttons.append([InlineKeyboardButton(f"{bot['username']}",
-                         callback_data=f"settings#editbots_{bot['username']}")])
+                         callback_data=f"settings#editbots_{bot['bot_id']}")])
      buttons.append([InlineKeyboardButton('✚ ᴀᴅᴅ Bᴏᴛꜱ ✚', 
                       callback_data="settings#addbots")])
      buttons.append([InlineKeyboardButton('🔙 Back', 
@@ -97,9 +97,8 @@ async def settings_query(bot, query):
                                          
                                              
   elif type.startswith("editbots"):
-     username = type.split('_')[1]
-     bot = await db.get_bots(user_id, bot_id, username)
-     bot_id = await db.get_bots(user_id, bot_id)
+     bot_id = type.split('_')[1]
+     bot = await db.get_bots(user_id, bot_id)
      buttons = [[InlineKeyboardButton('❌ Remove ❌', callback_data=f"settings#removebot_{bot_id}")
                ],
                [InlineKeyboardButton('🔙 Back', callback_data="settings#channels")]]
