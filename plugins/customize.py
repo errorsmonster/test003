9ce21964fb5cb22049c35f1644c934d1e607ed56
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-05-28 18:14:14 +0530

    Update customize.py

--- plugins/customize.py
+++ plugins/customize.py
@@ -21,8 +21,9 @@ async def shortlink(bot, message):
             "Format: <code>/force_sub bot_id -1002127267733</code></b>"
         )
     reply = await message.reply_text("<b>ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ...</b>")
-    if not re.match(r'\d{8,10}', bot_id):
-        return await reply.edit_text("<b>Invalid bot ID format! Bot ID must be between 8 and 10 digits.</b>")
+    bot_id = re.findall(r'\d{8,10}', bot_id):
+    if not bot_id:
+        return await reply.edit_text("<b>Invalid force ID provided!</b>")
     if await db.get_bot(user_id, bot_id) is None:
         return await reply.edit_text("<b>The specified bot ID does not belong to you!</b>")
     forc_id = re.findall(r'-\d{13}', forc_id)
